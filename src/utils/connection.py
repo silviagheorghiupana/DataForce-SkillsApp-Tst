a4d13bda5adbd839e4d5b47a3e30ea367445c31b
Silvia <silviagheorghiupana@gmail.com>
2022-08-27 20:39:46 +0300

    Initial commit

---
+++ src/utils/connection.py
@@ -0,0 +1,4 @@
+class MongoConnect:
+
+
+class MysqlConnect:
---
+++ src/utils/settings.py
@@ -0,0 +1,21 @@
+class Configuration:
+    @property
+    mongoConfiguration
+
+    @property
+    mysqlConfiguration
+    def __init__(self, connectionDetails):    #creates the object
+         self.connectionDetails = connectionDetails # a dictionary
+
+         
+
+
+
+class SettingApp:
+     def loadCfgFile:
+        # establish what cfg will be used
+        # creates a Configuration object with the details
+
+
+
+
